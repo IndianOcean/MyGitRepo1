9a04da42dfbd45eda7b1681f525da963e4d166bf
IndianOcean <dholamahesh@gmail.com>
2015-03-08 08:19:08 +0530

    changed

---
+++ MyGitRepo2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyGitRepo2.MvcApplication" Language="C#" %>
---
+++ MyGitRepo2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyGitRepo2.Startup))]
+namespace MyGitRepo2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
